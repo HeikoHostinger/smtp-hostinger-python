99ba2d97f8e3f37edf5373686590b36ba513a4bf
Heiko Häng <heiko@hostinger.com>
2022-01-17 10:15:33 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import smtp_hostinger
 
-smtp = smtp_hostinger.SmtpHostinger()
+smtp = smtp_hostinger.SMTPHostinger()
 smtp.auth("sender@hostinger.com", "Password123", "smtp.hostinger.com", 465, False)
 smtp.send("recipient@hostinger.com", "sender@hostinger.com", "This means that emails work", "Message body...")
